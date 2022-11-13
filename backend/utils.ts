0487422ff60fcff03f1cb2fdb9b4baeda41e88e7
Stan <stephenppercival@gmail.com>
2022-11-13 14:10:49 +0000

    change type in handle try catch

--- backend/utils.ts
+++ backend/utils.ts
@@ -1,8 +1,8 @@
 import { NextApiResponse } from "next";
 
-export async function handleTryCatch<T>(requestAction: Promise<T>, res: NextApiResponse<any>): Promise<void> {
+export async function handleTryCatch<T>(request: Promise<T>, res: NextApiResponse<any>): Promise<void> {
 	try {
-		const response = await requestAction;
+		const response = await request;
 		res.json(response);
 	} catch (error: any) {
 		res.status(500).json({ error: error.message });
